3428cdcb3183d99ea8bec0878d3359b0b18a1c9d
Ky23r <truowngfxuaan@gmail.com>
2026-01-05 09:44:06 +0700

    update README.md

--- seed-data/01-payment-status.sql
+++
@@ -1,7 +0,0 @@
--- Payment Status seed data
--- Run this after the application starts and tables are created
-
-INSERT INTO payment (status) VALUES ('PAID');
-INSERT INTO payment (status) VALUES ('UNPAID');
-INSERT INTO payment (status) VALUES ('VERIFYING');
-INSERT INTO payment (status) VALUES ('PAYMENT_FAILED');
