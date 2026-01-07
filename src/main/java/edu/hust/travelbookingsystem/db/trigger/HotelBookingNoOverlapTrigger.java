055a0d684e892eb20c1e4bbfead4bd0c8fbd14d4
vhm1605 <vhm1605@gmail.com>
2026-01-07 22:26:45 +0700

    small change

--- src/main/java/edu/hust/travelbookingsystem/db/trigger/HotelBookingNoOverlapTrigger.java
+++ src/main/java/edu/hust/travelbookingsystem/db/trigger/HotelBookingNoOverlapTrigger.java
@@ -41,7 +41,8 @@ public class HotelBookingNoOverlapTrigger implements Trigger {
                         (id == null ? "" : "  AND hb.id <> ?");
 
         try (PreparedStatement ps = conn.prepareStatement(sql)) {
-            int i = 1;
+            int i = 0;
+            i = 1;
             ps.setLong(i++, hotelId);
             ps.setLong(i++, bedroomId);
 
@@ -50,6 +51,8 @@ public class HotelBookingNoOverlapTrigger implements Trigger {
             ps.setDate(i++, java.sql.Date.valueOf(end));
 
             if (id != null) {
+
+
                 ps.setLong(i, id);
             }
 
